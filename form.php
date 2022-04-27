4f2cb8363fce85de38e065a52f041e4488fc17e5
mbelenluna <mariabelenluna18@gmail.com>
2022-04-27 01:40:57 -0300

    Fixed php2

--- form.php
+++ form.php
@@ -1,17 +1,17 @@
 <?php
 
-$nombre = $_POST ['nombre'];
-$email = $_POST ['email'];
-$telefono = $_POST ['telefono'];
-$idiomafuente = $_POST ['source-language'];
-$idiomaobjetivo = $_POST ['target-language'];
-$mensaje = $_POST ['message'];
+    $nombre = $_POST ['nombre'];
+    $email = $_POST ['email'];
+    $telefono = $_POST ['telefono'];
+    $idiomafuente = $_POST ['source-language'];
+    $idiomaobjetivo = $_POST ['target-language'];
+    $mensaje = $_POST ['message'];
 
-$para = 'rolltranslations@gmail.com';
-$asunto = 'New Submission from Contact Us';
+    $para = 'rolltranslations@gmail.com';
+    $asunto = 'New Submission from Contact Us';
 
-mail($para, $asunto, utf8_decode($mensaje));
+    mail($para, $asunto, utf8_decode($mensaje));
 
-header('Location:exito.html')
+    header('Location:exito.html');
 
 ?>

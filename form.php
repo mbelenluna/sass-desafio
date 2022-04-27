bcc71c8d6cfad30cf7482eef443b0d98c7227e20
mbelenluna <mariabelenluna18@gmail.com>
2022-04-26 23:52:42 -0300

    Fixed form

--- form.php
+++ form.php
@@ -3,12 +3,12 @@
 $nombre = $_POST ['nombre'];
 $email = $_POST ['email'];
 $telefono = $_POST ['telefono'];
-$idiomafuente = $_POST ['source-language']
-$idiomaobjetivo = $_POST ['target-language']
-$mensaje = $_POST ['message']
+$idiomafuente = $_POST ['source-language'];
+$idiomaobjetivo = $_POST ['target-language'];
+$mensaje = $_POST ['message'];
 
 $para = 'rolltranslations@gmail.com';
-$asunto = 'New Submission from Contact Us'
+$asunto = 'New Submission from Contact Us';
 
 mail($para, $asunto, utf8_decode($mensaje));
 

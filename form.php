2e59751bf63921b4c6bb3bbcb3679036cdb5ffaa
mbelenluna <mariabelenluna18@gmail.com>
2022-04-29 18:02:26 -0300

    Fixed form

--- form.php
+++ form.php
@@ -1,17 +1,26 @@
 <?php
 
-    $nombre = $_POST ['nombre'];
+if (isset($_POST['submit'])) {
+    
+    $fullname = $_POST ['fullname'];
     $email = $_POST ['email'];
-    $telefono = $_POST ['telefono'];
-    $idiomafuente = $_POST ['source-language'];
-    $idiomaobjetivo = $_POST ['target-language'];
-    $mensaje = $_POST ['message'];
+    $phonenumber = $_POST ['phonenumber'];
+    $servicerequested = $_POST ['servicerequested'];
+    $sourcelanguage = $_POST ['sourcelanguage'];
+    $targetlanguage = $_POST ['targetlanguage'];
+    $file = $_POST ['file'];
+    $message = $_POST ['message'];
 
     $para = 'rolltranslations@gmail.com';
     $asunto = 'New Submission from Contact Us';
+    $header = "From: ".$email;
+    $txt = "You have received an email from ".$fullname".\n\n".$email"\n".$phonenumber"\n".$servicerequested"\n".$sourcelanguage"\n"$targetlanguage"\n".$file"\n".$message;
 
-    mail($para, $asunto, utf8_decode($mensaje));
 
-    header('Location:success.html');
+    mail($para, $asunto, $txt, $header);
+
+    header('Location: success.html');
+
+}
 
 ?>

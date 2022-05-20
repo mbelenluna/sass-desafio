c6876f5a1016eebbf003b19e9b0c8f2c6330e01a
mbelenluna <mariabelenluna18@gmail.com>
2022-05-20 14:23:05 -0300

    Fixed SEO

--- pages/form.php
+++ pages/form.php
@@ -1,26 +1,22 @@
 <?php
 
-if (isset($_POST['submit'])) {
-    
-    $fullname = $_POST ['fullname'];
-    $email = $_POST ['email'];
-    $phonenumber = $_POST ['phonenumber'];
-    $servicerequested = $_POST ['servicerequested'];
-    $sourcelanguage = $_POST ['sourcelanguage'];
-    $targetlanguage = $_POST ['targetlanguage'];
-    $file = $_POST ['file'];
-    $message = $_POST ['message'];
+$fullname = $_POST ['fullname'];
+$email = $_POST ['email'];
+$phonenumber = $_POST ['phonenumber'];
+$servicerequested = $_POST ['servicerequested'];
+$sourcelanguage = $_POST ['sourcelanguage'];
+$targetlanguage = $_POST ['targetlanguage'];
+$file = $_POST ['file'];
+$message = $_POST ['message'];
 
-    $para = 'rolltranslations@gmail.com';
-    $asunto = 'New Submission from Contact Us';
-    $header = "From: ".$email;
-    $txt = "You have received an email from ".$fullname.$email.$phonenumber.$servicerequested.$sourcelanguage.$targetlanguage.$file.$message;
+$para = 'rolltranslations@gmail.com';
+$asunto = 'New Submission from Contact Us';
+$header = "From: ".$email;
+$txt = "You have received an email from ".$fullname.$email.$phonenumber.$servicerequested.$sourcelanguage.$targetlanguage.$file.$message;
 
 
-    mail($para, $asunto, $txt, $header);
+mail($para, $asunto, utf8_decode($txt), $header);
 
-    header('Location:success.html');
-
-}
+header('Location:success.html');
 
 ?>
